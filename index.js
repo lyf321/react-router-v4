4248b8c75c3ff61c1ec1c3840a8a98a6f937c576
lyf <2531349593@qq.com>
2017-08-22 14:16:23 +0800

    commit all

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by liyangfan on 17-8-16.
+ */
+import React from 'react'
+import {render} from 'react-dom'
+import BasicExample from './modules/A'
+render((
+<BasicExample/>
+), document.getElementById('app'))
---
+++ modules/Home.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liyangfan on 17-8-16.
+ */
+import React from 'react'
+
+export default React.createClass({
+    render() {
+        return <div>Home</div>
+    }
+})
